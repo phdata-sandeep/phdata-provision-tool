b39dd31f2b9cec10bfaf2a6e6fb10fee0eb32b54
smorcom <smorcom.phdata@conseroglobal.com>
2024-06-28 10:36:29 -0400

    Changes to resource monitors to only notify. Changed snowflake alerts script.

--- Manual_scripts/snowflake_alerts.sql
+++ Manual_scripts/snowflake_alerts.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES()
-RETURNS TABLE()
+RETURNS TABLE ()
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 
 BEGIN
 LET res RESULTSET := (
@@ -10,20 +10,18 @@ LET res RESULTSET := (
 FROM snowflake.account_usage.login_history
 WHERE 
 EVENT_TIMESTAMP >= dateadd(DAY, -1, current_timestamp())
-AND IS_SUCCESS = 'NO'
+AND IS_SUCCESS = ''NO''
 GROUP BY USER_NAME
 HAVING COUNT(*) > 5
 );
 RETURN TABLE(res);
 END;
-$$;
-
-
+';
 CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES_EMAIL()
-RETURNS VARCHAR
+RETURNS VARCHAR(16777216)
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 DECLARE
 
   c1 CURSOR FOR WITH email_body AS (
@@ -32,46 +30,90 @@ DECLARE
 FROM snowflake.account_usage.login_history
 WHERE 
 EVENT_TIMESTAMP >= dateadd(DAY, -1, current_timestamp())
-AND IS_SUCCESS = 'NO'
+AND IS_SUCCESS = ''NO''
 GROUP BY USER_NAME
 HAVING COUNT(*) > 5
                   ) 
                   SELECT TO_VARCHAR(OBJECT_CONSTRUCT(*)) AS all_rows_string FROM query_wrapper
             )
-            SELECT listagg(all_rows_string, '\n\n') as email_body_text FROM email_body;
+            SELECT listagg(all_rows_string, ''\\n\\n'') as email_body_text FROM email_body;
     subj varchar;
     msg varchar;
   BEGIN
       OPEN c1;
       FETCH c1 INTO msg;
-      msg := 'TO VIEW THE OUTPUT OF THE ALERT, RUN: \n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES();' || '\n\n OUTPUT OF ALERT:\n' || msg;
-      subj := 'SNOWFLAKE ALERT - USERS_LOGIN_FAILURES - ACCOUNT ' || current_account() || ' - ' || current_timestamp();
-      call system$send_email('RM_EMAIL_INT',:subj,:msg);
+      msg := ''TO VIEW THE OUTPUT OF THE ALERT, RUN: \\n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES();'' || ''\\n\\n OUTPUT OF ALERT:\\n'' || msg;
+      subj := ''SNOWFLAKE ALERT - PRIORITY P2  - USERS_LOGIN_FAILURES - ACCOUNT PROD'' || current_account() || '' - '' || current_timestamp();
+      call system$send_email(''RM_EMAIL_INT'',''consero@phdata.io'':subj,:msg);
   END;
-$$;
-
-
-CREATE OR REPLACE ALERT CDOPS_STATESTORE.MONITORING.USERS_LOGIN_FAILURES
-  WAREHOUSE = CDOPS_REPORT_WH
-  SCHEDULE = 'using cron 0 6 * * * America/Los_Angeles'
-  if (exists (
-        CALL CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES()
-     ))
-  THEN
-    CALL CDOPS_STATESTORE.MONITORING.SP_USERS_LOGIN_FAILURES_EMAIL();
-
-ALTER ALERT CDOPS_STATESTORE.MONITORING.USERS_LOGIN_FAILURES RESUME;
-
-CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET()
-RETURNS TABLE()
+';
+CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS()
+RETURNS TABLE ()
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 
 BEGIN
 LET res RESULTSET := (
       SELECT
-  'USER' AS OBJECT_TYPE,
+	LOGIN_NAME,
+	DISPLAY_NAME,
+	EMAIL,
+	LAST_SUCCESS_LOGIN
+FROM
+    SNOWFLAKE.ACCOUNT_USAGE.USERS
+WHERE
+    LAST_SUCCESS_LOGIN >= dateadd(DAY, -90, current_timestamp())
+    AND DELETED_ON IS NULL
+    AND DISABLED = FALSE
+);
+RETURN TABLE(res);
+END;
+';
+CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS_EMAIL()
+RETURNS VARCHAR(16777216)
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
+DECLARE
+
+  c1 CURSOR FOR WITH email_body AS (
+                  WITH query_wrapper AS (
+                        SELECT
+	LOGIN_NAME,
+	DISPLAY_NAME,
+	EMAIL,
+	LAST_SUCCESS_LOGIN
+FROM
+    SNOWFLAKE.ACCOUNT_USAGE.USERS
+WHERE
+    LAST_SUCCESS_LOGIN >= dateadd(DAY, -90, current_timestamp())
+    AND DELETED_ON IS NULL
+    AND DISABLED = FALSE
+                  ) 
+                  SELECT TO_VARCHAR(OBJECT_CONSTRUCT(*)) AS all_rows_string FROM query_wrapper
+            )
+            SELECT listagg(all_rows_string, ''\\n\\n'') as email_body_text FROM email_body;
+    subj varchar;
+    msg varchar;
+  BEGIN
+      OPEN c1;
+      FETCH c1 INTO msg;
+      msg := ''TO VIEW THE OUTPUT OF THE ALERT, RUN: \\n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS();'' || ''\\n\\n OUTPUT OF ALERT:\\n'' || msg;
+      subj := ''SNOWFLAKE ALERT - PRIORITY P2 - USERS_WITHOUT_LOG_IN_FOR_90_DAYS - ACCOUNT PROD '' || current_account() || '' - '' || current_timestamp();
+      call SYSTEM$SEND_EMAIL(''RM_EMAIL_INT'',''consero@phdata.io'',:subj,:msg);
+  END;
+';
+CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET()
+RETURNS TABLE ()
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
+
+BEGIN
+LET res RESULTSET := (
+      SELECT
+  ''USER'' AS OBJECT_TYPE,
   NAME AS OBJECT_NAME,
   FIRST_NAME,
   LAST_NAME,
@@ -83,25 +125,23 @@ FROM
 WHERE
   PASSWORD_LAST_SET_TIME >= dateadd(DAY, -1, current_timestamp())
   AND HAS_PASSWORD
-  AND DISABLED = 'false'
+  AND DISABLED = ''false''
   AND DELETED_ON IS NULL
 );
 RETURN TABLE(res);
 END;
-$$;
-
-
+';
 CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET_EMAIL()
-RETURNS VARCHAR
+RETURNS VARCHAR(16777216)
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 DECLARE
 
   c1 CURSOR FOR WITH email_body AS (
                   WITH query_wrapper AS (
                         SELECT
-  'USER' AS OBJECT_TYPE,
+  ''USER'' AS OBJECT_TYPE,
   NAME AS OBJECT_NAME,
   FIRST_NAME,
   LAST_NAME,
@@ -113,112 +153,27 @@ FROM
 WHERE
   PASSWORD_LAST_SET_TIME >= dateadd(DAY, -30, current_timestamp())
   AND HAS_PASSWORD
-  AND DISABLED = 'false'
+  AND DISABLED = ''false''
   AND DELETED_ON IS NULL
                   ) 
                   SELECT TO_VARCHAR(OBJECT_CONSTRUCT(*)) AS all_rows_string FROM query_wrapper
             )
-            SELECT listagg(all_rows_string, '\n\n') as email_body_text FROM email_body;
+            SELECT listagg(all_rows_string, ''\\n\\n'') as email_body_text FROM email_body;
     subj varchar;
     msg varchar;
   BEGIN
       OPEN c1;
       FETCH c1 INTO msg;
-      msg := 'TO VIEW THE OUTPUT OF THE ALERT, RUN: \n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET();' || '\n\n OUTPUT OF ALERT:\n' || msg;
-      subj := 'SNOWFLAKE ALERT - USERS_WITH_A_PASSWORD_SET - ACCOUNT ' || current_account() || ' - ' || current_timestamp();
-      call system$send_email('RM_EMAIL_INT',:subj,:msg);
+      msg := ''TO VIEW THE OUTPUT OF THE ALERT, RUN: \\n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET();'' || ''\\n\\n OUTPUT OF ALERT:\\n'' || msg;
+      subj := ''SNOWFLAKE ALERT - PRIORITY P2 - USERS_WITH_A_PASSWORD_SET - ACCOUNT PROD '' || current_account() || '' - '' || current_timestamp();
+      call system$send_email(''RM_EMAIL_INT'',''consero@phdata.io'',:subj,:msg);
   END;
-$$;
-
-
-CREATE OR REPLACE ALERT CDOPS_STATESTORE.MONITORING.USERS_WITH_A_PASSWORD_SET
-  WAREHOUSE = CDOPS_REPORT_WH
-  SCHEDULE = 'using cron 0 6 * * * America/Los_Angeles'
-  if (exists (
-        CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET()
-     ))
-  THEN
-    CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITH_A_PASSWORD_SET_EMAIL();
-
-ALTER ALERT CDOPS_STATESTORE.MONITORING.USERS_WITH_A_PASSWORD_SET RESUME;
-
-CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS()
-RETURNS TABLE()
-LANGUAGE SQL
-AS
-$$
-
-BEGIN
-LET res RESULTSET := (
-      SELECT
-	LOGIN_NAME,
-	DISPLAY_NAME,
-	EMAIL,
-	LAST_SUCCESS_LOGIN
-FROM
-    SNOWFLAKE.ACCOUNT_USAGE.USERS
-WHERE
-    LAST_SUCCESS_LOGIN >= dateadd(DAY, -90, current_timestamp())
-    AND DELETED_ON IS NULL
-    AND DISABLED = FALSE
-);
-RETURN TABLE(res);
-END;
-$$;
-
-
-CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS_EMAIL()
-RETURNS VARCHAR
-LANGUAGE SQL
-AS
-$$
-DECLARE
-
-  c1 CURSOR FOR WITH email_body AS (
-                  WITH query_wrapper AS (
-                        SELECT
-	LOGIN_NAME,
-	DISPLAY_NAME,
-	EMAIL,
-	LAST_SUCCESS_LOGIN
-FROM
-    SNOWFLAKE.ACCOUNT_USAGE.USERS
-WHERE
-    LAST_SUCCESS_LOGIN >= dateadd(DAY, -90, current_timestamp())
-    AND DELETED_ON IS NULL
-    AND DISABLED = FALSE
-                  ) 
-                  SELECT TO_VARCHAR(OBJECT_CONSTRUCT(*)) AS all_rows_string FROM query_wrapper
-            )
-            SELECT listagg(all_rows_string, '\n\n') as email_body_text FROM email_body;
-    subj varchar;
-    msg varchar;
-  BEGIN
-      OPEN c1;
-      FETCH c1 INTO msg;
-      msg := 'TO VIEW THE OUTPUT OF THE ALERT, RUN: \n CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS();' || '\n\n OUTPUT OF ALERT:\n' || msg;
-      subj := 'SNOWFLAKE ALERT - USERS_WITHOUT_LOG_IN_FOR_90_DAYS - ACCOUNT ' || current_account() || ' - ' || current_timestamp();
-      call system$send_email('RM_EMAIL_INT',:subj,:msg);
-  END;
-$$;
-
-
-CREATE OR REPLACE ALERT CDOPS_STATESTORE.MONITORING.USERS_WITHOUT_LOG_IN_FOR_90_DAYS
-  WAREHOUSE = CDOPS_REPORT_WH
-  SCHEDULE = 'using cron 0 6 * * * America/Los_Angeles'
-  if (exists (
-        CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS()
-     ))
-  THEN
-    CALL CDOPS_STATESTORE.MONITORING.SP_USERS_WITHOUT_LOG_IN_FOR_90_DAYS_EMAIL();
-
-ALTER ALERT CDOPS_STATESTORE.MONITORING.USERS_WITHOUT_LOG_IN_FOR_90_DAYS RESUME;
-
+';
 CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR()
-RETURNS TABLE()
+RETURNS TABLE ()
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 
 DECLARE
 presql RESULTSET DEFAULT (show warehouses in account);
@@ -231,18 +186,16 @@ LET res RESULTSET := (
     "auto_suspend" 
 From TABLE(RESULT_SCAN(LAST_QUERY_ID())) 
 WHERE
-    "resource_monitor" = 'null'
+    "resource_monitor" = ''null''
 );
 RETURN TABLE(res);
 END;
-$$;
-
-
+';
 CREATE OR REPLACE PROCEDURE CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR_EMAIL()
-RETURNS VARCHAR
+RETURNS VARCHAR(16777216)
 LANGUAGE SQL
-AS
-$$
+EXECUTE AS OWNER
+AS '
 DECLARE
 
 presql RESULTSET DEFAULT (show warehouses in account);
@@ -255,30 +208,18 @@ presql RESULTSET DEFAULT (show warehouses in account);
     "auto_suspend" 
 From TABLE(RESULT_SCAN(LAST_QUERY_ID())) 
 WHERE
-    "resource_monitor" = 'null'
+    "resource_monitor" = ''null''
                   ) 
                   SELECT TO_VARCHAR(OBJECT_CONSTRUCT(*)) AS all_rows_string FROM query_wrapper
             )
-            SELECT listagg(all_rows_string, '\n\n') as email_body_text FROM email_body;
+            SELECT listagg(all_rows_string, ''\\n\\n'') as email_body_text FROM email_body;
     subj varchar;
     msg varchar;
   BEGIN
       OPEN c1;
       FETCH c1 INTO msg;
-      msg := 'TO VIEW THE OUTPUT OF THE ALERT, RUN: \n CALL CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR();' || '\n\n OUTPUT OF ALERT:\n' || msg;
-      subj := 'SNOWFLAKE ALERT - WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR - ACCOUNT ' || current_account() || ' - ' || current_timestamp();
-      call system$send_email('RM_EMAIL_INT',:subj,:msg);
+      msg := ''TO VIEW THE OUTPUT OF THE ALERT, RUN: \\n CALL CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR();'' || ''\\n\\n OUTPUT OF ALERT:\\n'' || msg;
+      subj := ''SNOWFLAKE ALERT - PRIORITY P2 - WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR - ACCOUNT PROD'' || current_account() || '' - '' || current_timestamp();
+      call system$send_email(''RM_EMAIL_INT'',''consero@phdata.io'',:subj,:msg);
   END;
-$$;
-
-
-CREATE OR REPLACE ALERT CDOPS_STATESTORE.MONITORING.WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR
-  WAREHOUSE = CDOPS_REPORT_WH
-  SCHEDULE = 'using cron 0 6 * * * America/Los_Angeles'
-  if (exists (
-        CALL CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR()
-     ))
-  THEN
-    CALL CDOPS_STATESTORE.MONITORING.SP_WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR_EMAIL();
-
-ALTER ALERT CDOPS_STATESTORE.MONITORING.WAREHOUSES_WITHOUT_A_RESOURCE_MONITOR RESUME;
+';
